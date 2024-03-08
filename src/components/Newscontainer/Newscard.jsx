215ae21f60d74fd7d2ac977a3befcbe713a74f06
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-03-08 12:27:33 +0530

    news container mobilre responsiveness fixed

--- src/components/Newscontainer/Newscard.jsx
+++ src/components/Newscontainer/Newscard.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Newscard = (props) => {
   return (
-        <div className='w-full flex min-h-[20vh] gap-8'>
-            <div className="newsimagecontainer w-[48vw] h-[20vw]">
+        <div className='w-full flex flex-col lg:flex-row min-h-[20vh] lg:gap-12 gap:6 border-b-2 border-zinc-400'>
+            <div className="newsimagecontainer w-full lg:w-[42vw] h-[40vw] lg:h-[20vw]">
                 <img src={props.img} className='w-full h-full'/>
             </div>
-            <div className="newsinfocontainer titlefont flex flex-col gap-2 justify-start items-start w-[50vw]">
-                <h4 className='titlefont'>{props.newsDate} / {props.author}</h4>
-                <h1 className='titlefont font-bold text-[#dc2626] text-[2.1vw]'>{props.newsTitle}</h1>
-                <p className='poppins-regular text-[1.2rem]'>{props.newsSnippet}</p>
-                <a href="" className='titlefont mt-4 hover:underline text-[1.6rem] text-[#dc2626]'>Read More</a>
+            <div className="newsinfocontainer titlefont flex flex-col gap-2 justify-start items-start w-full lg:w-[50vw]">
+                <h4 className='titlefont text-[0.6rem]'>{props.newsDate} / {props.author}</h4>
+                <h1 className='titlefont font-bold text-[#dc2626] lg:text-[2.1vw] text-[6vw] leading-[6vw]'>{props.newsTitle}</h1>
+                <p className='poppins-regular lg:text-[1.2rem] text-[0.7rem]'>{props.newsSnippet}</p>
+                <a href="" className='titlefont mt-0 lg:mt-4 hover:underline text-[.8rem] lg:text-[1.6rem] text-[#dc2626]'>Read More</a>
             </div>
         </div>
   )
--- src/components/Newscontainer/Newscontainer.jsx
+++ src/components/Newscontainer/Newscontainer.jsx
@@ -1,42 +1,52 @@
-import React from 'react'
-import Newscard from './Newscard'
+import React from "react";
+import Newscard from "./Newscard";
 const Newscontainer = () => {
   return (
     <div className="newsholder p-6 flex flex-col gap-4">
-        <h1 className='reikna text-[6vw] leading-[6vw]'>Featured</h1>
-        <Newscard img="https://images.unsplash.com/photo-1461696114087-397271a7aedc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            newsTitle="Random News Title, I could not be creative enough!"
-            newsDate="8th August 2023"
-            author="Nilaa Laishram"
-            newsSnippet="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos blanditiis atque possimus. Repellendus voluptatibus autem provident laboriosam, placeat, 
+      <h1 className="reikna uppercase text-[3rem] leading-[2rem] lg:text-[6vw] lg:leading-[6vw]">
+        Featured
+      </h1>
+      <Newscard
+        img="https://images.unsplash.com/photo-1461696114087-397271a7aedc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        newsTitle="Random News Title, I could not be creative enough!"
+        newsDate="8th August 2023"
+        author="Nilaa Laishram"
+        newsSnippet="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos blanditiis atque possimus. Repellendus voluptatibus autem provident laboriosam, placeat, 
             sed in ea error asperiores eum eveniet adipisci rerum, veniam veritatis unde.
-            Repellendus officiis, fugit ipsam molestiae quod veniam eos illo quo provident veritatis delectus blanditiis facere tempore qui cupiditate."/>
+            Repellendus officiis, fugit ipsam molestiae quod veniam eos illo quo provident veritatis delectus blanditiis facere tempore qui cupiditate."
+      />
 
-<Newscard img="https://images.unsplash.com/photo-1461696114087-397271a7aedc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            newsTitle="Random News Title, I could not be creative enough!"
-            newsDate="8th August 2023"
-            author="Nilaa Laishram"
-            newsSnippet="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos blanditiis atque possimus. Repellendus voluptatibus autem provident laboriosam, placeat, 
+      <Newscard
+        img="https://images.unsplash.com/photo-1498736297812-3a08021f206f?q=80&w=1779&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        newsTitle="Random News Title, I could not be creative enough!"
+        newsDate="8th August 2023"
+        author="Nilaa Laishram"
+        newsSnippet="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos blanditiis atque possimus. Repellendus voluptatibus autem provident laboriosam, placeat, 
             sed in ea error asperiores eum eveniet adipisci rerum, veniam veritatis unde.
-            Repellendus officiis, fugit ipsam molestiae quod veniam eos illo quo provident veritatis delectus blanditiis facere tempore qui cupiditate."/>
+            Repellendus officiis, fugit ipsam molestiae quod veniam eos illo quo provident veritatis delectus blanditiis facere tempore qui cupiditate."
+      />
 
-<Newscard img="https://images.unsplash.com/photo-1461696114087-397271a7aedc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            newsTitle="Random News Title, I could not be creative enough!"
-            newsDate="8th August 2023"
-            author="Nilaa Laishram"
-            newsSnippet="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos blanditiis atque possimus. Repellendus voluptatibus autem provident laboriosam, placeat, 
+      <Newscard
+        img="https://images.unsplash.com/photo-1461696114087-397271a7aedc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        newsTitle="Random News Title, I could not be creative enough!"
+        newsDate="8th August 2023"
+        author="Nilaa Laishram"
+        newsSnippet="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos blanditiis atque possimus. Repellendus voluptatibus autem provident laboriosam, placeat, 
             sed in ea error asperiores eum eveniet adipisci rerum, veniam veritatis unde.
-            Repellendus officiis, fugit ipsam molestiae quod veniam eos illo quo provident veritatis delectus blanditiis facere tempore qui cupiditate."/>
+            Repellendus officiis, fugit ipsam molestiae quod veniam eos illo quo provident veritatis delectus blanditiis facere tempore qui cupiditate."
+      />
 
-<Newscard img="https://images.unsplash.com/photo-1461696114087-397271a7aedc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            newsTitle="Random News Title, I could not be creative enough!"
-            newsDate="8th August 2023"
-            author="Nilaa Laishram"
-            newsSnippet="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos blanditiis atque possimus. Repellendus voluptatibus autem provident laboriosam, placeat, 
+      <Newscard
+        img="https://images.unsplash.com/photo-1461696114087-397271a7aedc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        newsTitle="Random News Title, I could not be creative enough!"
+        newsDate="8th August 2023"
+        author="Nilaa Laishram"
+        newsSnippet="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos blanditiis atque possimus. Repellendus voluptatibus autem provident laboriosam, placeat, 
             sed in ea error asperiores eum eveniet adipisci rerum, veniam veritatis unde.
-            Repellendus officiis, fugit ipsam molestiae quod veniam eos illo quo provident veritatis delectus blanditiis facere tempore qui cupiditate."/>
+            Repellendus officiis, fugit ipsam molestiae quod veniam eos illo quo provident veritatis delectus blanditiis facere tempore qui cupiditate."
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Newscontainer
+export default Newscontainer;
