72bf3aa6fc50d6f9cad50b33062dd9c2194ce606
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-03-08 20:21:51 +0530

    minor news card ui changes

--- src/components/Newscontainer/Newscard.jsx
+++ src/components/Newscontainer/Newscard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Newscard = (props) => {
   return (
-        <div className='w-full flex flex-col lg:flex-row min-h-[20vh] lg:gap-12 gap:6 border-b-2 border-zinc-400'>
+        <div className='w-full flex flex-col lg:flex-row min-h-[20vh] lg:gap-12 py-4 gap:6 border-b-2 border-zinc-400'>
             <div className="newsimagecontainer w-full lg:w-[42vw] h-[40vw] lg:h-[20vw]">
                 <img src={props.img} className='w-full h-full'/>
             </div>
             <div className="newsinfocontainer titlefont flex flex-col gap-2 justify-start items-start w-full lg:w-[50vw]">
-                <h4 className='titlefont text-[0.6rem]'>{props.newsDate} / {props.author}</h4>
-                <h1 className='titlefont font-bold text-[#dc2626] lg:text-[2.1vw] text-[6vw] leading-[6vw]'>{props.newsTitle}</h1>
+                <h4 className='titlefont lg:text-[1rem] text-[0.6rem]'>{props.newsDate} / {props.author}</h4>
+                <h1 className='titlefont font-bold text-[#dc2626] lg:text-[2.1vw] text-[6vw] lg:leading-[2vw] leading-[6vw]'>{props.newsTitle}</h1>
                 <p className='poppins-regular lg:text-[1.2rem] text-[0.7rem]'>{props.newsSnippet}</p>
                 <a href="" className='titlefont mt-0 lg:mt-4 hover:underline text-[.8rem] lg:text-[1.6rem] text-[#dc2626]'>Read More</a>
             </div>
--- src/components/Newscontainer/Newscontainer.jsx
+++ src/components/Newscontainer/Newscontainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Newscard from "./Newscard";
 const Newscontainer = () => {
   return (
-    <div className="newsholder p-6 flex flex-col gap-4">
+    <div className="newsholder p-6 flex flex-col">
       <h1 className="reikna uppercase text-[3rem] leading-[2rem] lg:text-[6vw] lg:leading-[6vw]">
         Featured
       </h1>
