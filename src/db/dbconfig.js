e9d235c829aa47e2527ec43759201d28a3ff8c39
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-03-10 21:09:50 +0530

    set up db and storage

--- src/db/dbconfig.js
+++ src/db/dbconfig.js
@@ -1,5 +1,5 @@
 import config from "../configs/config";
-import { Client, Databases, Storage, Query } from "appwrite";
+import { Client, Databases, Storage, Query, ID } from "appwrite";
 
 export class DBService {
   client = new Client();
@@ -13,7 +13,7 @@ export class DBService {
     this.databases = new Databases(this.client);
     this.bucket = new Storage(this.client);
   }
-  async getPost(slug) {
+  async getNewsPost(slug) {
     try {
       this.databases.getDocument(
         config.appwriteDatabaseID,
@@ -21,11 +21,12 @@ export class DBService {
         slug
       );
     } catch (error) {
-      console.log("Appwrite Service :: getPost() ::", error);
+      console.log("Appwrite Service :: getNewsPost() ::", error);
+      return false;
     }
   }
 
-  async getPosts(queries = [Query.equal("status", "active")]) {
+  async getNews(queries = [Query.equal("status", "active")]) {
     try {
       return await this.databases.listDocuments(
         config.appwriteDatabaseID,
@@ -33,8 +34,130 @@ export class DBService {
         queries
       );
     } catch (error) {
-      console.log("Appwrite Service :: getPosts() ::", error);
+      console.log("Appwrite Service :: getNews() ::", error);
+      return false;
     }
   }
-  
+
+  async createNews(
+    slug,
+    {
+      title,
+      author,
+      createdOn,
+      newsCategory,
+      newsContent,
+      newsImage1,
+      newsImage2,
+      authorId,
+      status,
+    }
+  ) {
+    try {
+      return await this.databases.createDocument(
+        config.appwriteDatabaseID,
+        config.appwriteCollectionID,
+        slug,
+        {
+          title,
+          author,
+          createdOn,
+          newsCategory,
+          newsContent,
+          newsImage1,
+          newsImage2,
+          authorId,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite Service :: createNews() ::", error);
+      return false;
+    }
+  }
+
+  async updateNews(
+    slug,
+    {
+      title,
+      author,
+      createdOn,
+      newsCategory,
+      newsContent,
+      newsImage1,
+      newsImage2,
+      status,
+      isFeatured,
+      isBreaking,
+      isPopular,
+      isEditorsChoice,
+    }
+  ) {
+    try {
+      return await this.databases.updateDocument(
+        config.appwriteDatabaseID,
+        config.appwriteCollectionID,
+        slug,
+        {
+          title,
+          author,
+          createdOn,
+          newsCategory,
+          newsContent,
+          newsImage1,
+          newsImage2,
+          status,
+          likes,
+          dislikes,
+          views,
+          isFeatured,
+          isBreaking,
+          isPopular,
+          isEditorsChoice,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite Service :: updateNews() ::", error);
+      return false;
+    }
+  }
+
+  async deleteNews(slug) {
+    try {
+      await this.databases.deleteDocument(
+        config.appwriteDatabaseID,
+        config.appwriteCollectionID,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite Service :: deleteNews() ::", error);
+      return false;
+    }
+  }
+
+  async uploadFiles(file) {
+    try {
+      return await this.bucket.createFile(config.appwriteBucketID, ID.unique());
+    } catch (error) {
+      console.log("Appwrite Service :: uploadFiles() ::", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      return await this.bucket.createFile(config.appwriteBucketID, fileId);
+    } catch (error) {
+      console.log("Appwrite Service :: uploadFiles() ::", error);
+      return false;
+    }
+  }
+
+  getfilePreview(fileId){
+    return this.bucket.getFilePreview(
+      config.appwriteBucketID,
+      fileId
+    ).href
+  }
 }
